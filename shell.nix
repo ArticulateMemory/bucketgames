4b01a61ead2a4c24f20fd706a027ff86e66cc125
Josie <85971228+ArticulateMemory@users.noreply.github.com>
2026-01-10 14:45:52 -0500

    PurgeCSS initial add

--- shell.nix
+++ shell.nix
@@ -2,11 +2,18 @@
 
 
 pkgs.mkShellNoCC {
+    name="pyBucketGames-Shell";
+    
     packages = with pkgs; [
-       git uv python313
+       git uv python313 
+       nodejs dart-sass
     ];
-
-#     shellHook = ''
-#         echo "Available commands: git, uv"
-#     '';
+    
+    BASE_PATH = builtins.getEnv "PWD";
+    
+    shellHook = ''
+        alias updatecss="npm i && echo;echo;echo;echo 'To update minified css, please run \"npx purgecss --css $BASE_PATH/src/pybucketgames/_static/bootstrap.min.css --content $BASE_PATH/src/pybucketgames/templates/*.html $BASE_PATH/*/bucket.html $BASE_PATH/*/game.html $BASE_PATH/*/*/bucket.html $BASE_PATH/*/*/game.html $BASE_PATH/*/_website/**/*.html $BASE_PATH/*/_website/*.html --output $BASE_PATH/src/pybucketgames/_static/purgecss/\"'"
+        alias pybucketgames="uv run pybucketgames";
+        echo "Commands: pybucketgames, updatecss, git, uv";
+    '';
 }
