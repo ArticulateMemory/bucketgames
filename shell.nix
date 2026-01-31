198bf53f5c3e93ea2520e389682a7eafc1c6d995
Josie <85971228+ArticulateMemory@users.noreply.github.com>
2026-01-31 17:36:39 -0500

    Improve updatecss command to perform to action. Rebase.

--- shell.nix
+++ shell.nix
@@ -12,8 +12,12 @@ pkgs.mkShellNoCC {
     BASE_PATH = builtins.getEnv "PWD";
     
     shellHook = ''
-        alias updatecss="npm i && echo;echo;echo;echo 'To update minified css, please run \"npx purgecss --css $BASE_PATH/src/pybucketgames/_static/bootstrap.min.css --content $BASE_PATH/src/pybucketgames/templates/*.html $BASE_PATH/*/bucket.html $BASE_PATH/*/game.html $BASE_PATH/*/*/bucket.html $BASE_PATH/*/*/game.html $BASE_PATH/*/_website/**/*.html $BASE_PATH/*/_website/*.html --output $BASE_PATH/src/pybucketgames/_static/purgecss/\"'"
+        mkdir -p $BASE_PATH/src/pybucketgames/_static/purgecss
+        
+        alias updatecss="npm i && npx purgecss --css $BASE_PATH/src/pybucketgames/_static/bootstrap.min.css --content $BASE_PATH/src/pybucketgames/templates/*.html $BASE_PATH/*/bucket.html $BASE_PATH/*/game.html $BASE_PATH/*/*/bucket.html $BASE_PATH/*/*/game.html $BASE_PATH/*/_website/**/*.html $BASE_PATH/*/_website/*.html --output $BASE_PATH/src/pybucketgames/_static/purgecss"
+        
         alias pybucketgames="uv run pybucketgames";
+        
         echo "Commands: pybucketgames, updatecss, git, uv";
     '';
 }
